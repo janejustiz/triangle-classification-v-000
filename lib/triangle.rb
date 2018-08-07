6a35344b3b3e141f63cf750e623c198301590ebe
Jane Justiz <janejustiz@gmail.com>
2018-08-07 14:36:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+  def kind
+  end
 end
